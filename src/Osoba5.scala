de1a3d7eda43a2ac99e5f803132d91197abe67bb
Jan Stojaczyk <jan.stojaczyk@gmail.com>
2020-03-22 14:16:10 +0100

    Final changes

--- src/Osoba5.scala
+++ src/Osoba5.scala
@@ -2,13 +2,13 @@ abstract class Osoba5 (val imie:String, val nazwisko:String){
   val podatek:Double
 }
 
-trait Pracownik {
+trait Pracownik extends Osoba5{
  var pensja:Double = 2020.99
-  val podatek = 0.2*pensja
+ override val podatek = 0.2*pensja
 }
 
-trait Student {
-  val podatek: Double = 0.0
+trait Student extends Osoba5{
+  override val podatek: Double = 0.0
 }
 
 trait Nauczyciel extends Pracownik{
