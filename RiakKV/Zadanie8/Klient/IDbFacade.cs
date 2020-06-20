e589fb81d72953d07980bca4cde1e789a66aecd1
Jan Stojaczyk <s21748@pjwstk.edu.pl>
2020-06-20 12:40:20 +0200

    something ready

---
+++ RiakKV/Zadanie8/Klient/IDbFacade.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace DefaultNamespace
+{
+    public interface IDbFacade<T>
+    {
+        Task<string> AddAsync(T newValue);
+        Task AddAsync(T newValue, string key);
+
+        Task UpdateAsync(T newValue, string key);
+
+        Task RemoveAsync(string key);
+    }
+}
---
+++ RiakKV/Zadanie8/Klient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Klient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
